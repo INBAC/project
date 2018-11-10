07916c53d3d42e6b806147dee65cacf1454bbae3
LeeDongu <scudys@naver.com>
2018-11-10 16:59:42 +0900

    NEW added
    New Added

---
+++ New.c
@@ -0,0 +1,132 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<time.h>
+#include<signal.h>
+#include<errno.h>
+#include<sys/time.h>
+#include<sys/types.h>
+#include<sys/wait.h>
+#include"queue1.h"
+
+#define CPU_TIME_QUANTUM 3
+#define USER_PROCESS_NUM 5
+
+void parent_handler(int signo);
+void child_action(int cpu_burst);
+void child_handler(int signo);
+PCB* scheduler();
+
+struct PCB *pcb[USER_PROCESS_NUM];		// For All pcb
+struct PCB *present_pcb;	// For present job
+struct Queue *readyQueue;		// main queue
+
+int remain_cpu_burst;
+int cpu_burst[USER_PROCESS_NUM];
+
+int main()
+{
+	pid_t pid;
+	int i;
+
+	struct sigaction old_sa, new_sa;
+	struct itimerval new_itimer, old_itimer;
+
+	readyQueue = (Queue*)malloc(sizeof(Queue));
+	readyQueue = CreateQueue();
+
+	srand((int)time(NULL));
+	for(int i = 0; i < USER_PROCESS_NUM ; i++) {
+		cpu_burst[i] = (rand() % 5) + 1;
+		pid = fork();
+		if (pid < -1) {
+			perror("fork error");
+			return 0;
+			}
+		else if (pid == 0) {
+			// child
+			printf("child process with pid %d\n", getpid());
+			child_action(cpu_burst[i]);
+			}
+		else {
+			// parent
+			printf("my pid is %d\n", getpid());
+			pcb[i] = (PCB*)malloc(sizeof(PCB));
+			memset(pcb[i], 0, sizeof(PCB));
+			pcb[i]->pid = pid;
+			pcb[i]->remain_CPU_burst = cpu_burst[i];
+			pcb[i]->remain_CPU_TIME_QUANTUM = CPU_TIME_QUANTUM;
+			addprocess(readyQueue, pcb[i]);
+			}
+		}
+
+	memset(&new_sa, 0, sizeof(new_sa));
+	new_sa.sa_handler = &parent_handler;
+	sigaction(SIGALRM, &new_sa, &old_sa);
+
+	//timer setting
+	new_itimer.it_interval.tv_sec = 1;
+	new_itimer.it_interval.tv_usec = 0;
+	new_itimer.it_value.tv_sec = 1;
+	new_itimer.it_value.tv_usec = 0;
+	setitimer(ITIMER_REAL, &new_itimer, &old_itimer); // First signal은 1초후에 울림
+
+	while (present_pcb -> whto != 0);  //while loop를 까먹으면 signal이고 뭐고 아무것도 없다
+	printf("zzzz fake \n");
+	exit(0);
+}
+
+void parent_handler(int signo){
+	PCB *next_process_pcb = NULL;
+
+	if(present_pcb == NULL){
+		present_pcb = scheduler();
+	}
+
+	present_pcb -> remain_CPU_TIME_QUANTUM--;
+	present_pcb -> remain_CPU_burst--;
+	if(present_pcb -> remain_CPU_burst != 0){
+		if(present_pcb -> remain_CPU_TIME_QUANTUM == 0){
+			present_pcb -> remain_CPU_TIME_QUANTUM = CPU_TIME_QUANTUM;
+			removeprocess(readyQueue, present_pcb);
+			addprocess(readyQueue, present_pcb);
+			if((next_process_pcb = scheduler()) != NULL){
+				present_pcb = next_process_pcb;
+			}
+		}
+	}
+	else{
+		removeprocess(readyQueue, present_pcb);
+		if((next_process_pcb = scheduler()) != NULL){
+			present_pcb = next_process_pcb;
+		}
+	}
+	kill(present_pcb -> pid, SIGALRM);
+}
+
+void child_action(int cpu_burst)
+{
+	struct sigaction oldchild_sa, newchild_sa;
+	remain_cpu_burst = cpu_burst;
+
+	memset(&newchild_sa, 0, sizeof(newchild_sa));
+	newchild_sa.sa_handler = &child_handler;
+	sigaction(SIGALRM, &newchild_sa, &oldchild_sa);		/// ALARM 종류를 어떤 기준으로 설정 할 것인가?
+	while(remain_cpu_burst > 0);
+	printf("PID %d EXIT\n", getpid());
+	exit(0);
+}
+
+void child_handler(int signo){
+	printf("Proc (%d) remaining cpu time is : %d\n", getpid(), remain_cpu_burst);
+	remain_cpu_burst--;
+}
+
+PCB* scheduler(){
+	if(readyQueue->count != 0)			//만약 readyQueue (readyquque)에 카운트가 0이 아니라면 -> 뭐라도 잇다면
+		return readyQueue->head->pcb;
+	else
+		return NULL;
+}
+
---
+++ queue1.h
@@ -0,0 +1,106 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define FALSE 0
+#define TRUE 1
+
+typedef struct PCB{
+	int pid;
+
+	int IO_burst;
+	int remain_IO_burst;
+	int remain_CPU_TIME_QUANTUM;
+} PCB;
+
+typedef struct PROCESS{
+		PCB *pcb;
+		struct PROCESS *next;
+} PROCESS;
+
+typedef struct Queue{
+	int count;
+	PROCESS *head;
+	PROCESS *pos;
+} Queue;
+
+Queue *CreateQueue(){
+	Queue *NewQueue = (Queue*)malloc(sizeof(Queue));
+	NewQueue->pos = NULL;
+	NewQueue->head = NULL; //initialize
+	NewQueue->count = 0;
+	return NewQueue;
+}
+
+int SearchQueue(Queue *p, PROCESS **ppPre, PROCESS **ppLoc, PCB *pcb){
+	for(*ppPre = NULL, *ppLoc = p->head; *ppLoc != NULL; *ppPre = *ppLoc, *ppLoc = (*ppLoc)->next){
+		if((*ppLoc)->pcb->pid == pcb->pid)
+			return TRUE;
+	//	else if((*ppLoc)->pcb->pid > pcb->pid)
+	//		break;
+	}
+	return FALSE;
+}
+
+void insertQueue(Queue *p, PROCESS *pPre, PCB *pcb){
+	PROCESS *New_process = (PROCESS*)malloc(sizeof(PROCESS));
+	New_process->pcb = pcb;
+
+	if(pPre == NULL){
+		New_process->next = p->head;
+		p->head = New_process;
+	}
+	else{
+		New_process->next = pPre->next;
+		pPre->next = New_process;
+	}
+	p->count++;
+}
+
+PCB *deleteQueue(Queue *p, PROCESS *pPre, PROCESS *pLoc){
+	PCB *temp = NULL;
+	temp = pLoc->pcb;
+
+	if(pPre == NULL)
+		p->head = pLoc->next;
+	else
+		pPre->next = pLoc->next;
+
+	free(pLoc);
+	p->count--;
+	return temp;
+}
+
+void addprocess(Queue *p, PCB *pcb){
+	PROCESS *pPre = NULL, *pLoc = NULL;
+	int found;
+	found = SearchQueue(p, &pPre, &pLoc, pcb);
+	if(!found)
+		insertQueue(p, pPre, pcb);
+}
+
+PCB* removeprocess(Queue *p, PCB *pcb){
+	PROCESS *pPre = NULL, *pLoc = NULL;
+	PCB *temp = NULL;
+	int found;
+
+	found = SearchQueue(p, &pPre, &pLoc, pcb);
+	if(found)
+		temp = deleteQueue(p, pPre, pLoc);
+	return temp;
+}
+
+void destroyQueue(Queue *p){
+	PROCESS *pDel = NULL, *pNext = NULL;
+	for(pDel = p->head; pDel != NULL; pDel = pNext){
+		pNext = pDel->next;
+		free(pDel);
+	}
+	free(p);
+}
+/*
+void priority_queue(Queue *p, PCB *pcb){
+	pcb->remain_IO_burst;
+
+
+}
+*/
